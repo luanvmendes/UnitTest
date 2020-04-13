42027ce490806ef53e2f2f1fded55cd8b952f39c
luanvmendes <luan_vmendes@hotmail.com>
2020-04-12 21:17:20 -0300

    Testes unitários

---
+++ testclass.cs
@@ -0,0 +1,28 @@
+using Xunit;
+
+namespace UnitTests
+{
+    public class testclass
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int mynumber)
+        {
+            Assert.True(Program.IsOdd(mynumber));
+        }
+
+        [Fact]
+        public void PassingAddTest()
+        {
+            Assert.Equal(4, Program.Add(2,2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Program.Add(2,2));
+        }
+    }
+}
